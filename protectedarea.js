aac6ea6bcbd535e0983ae39a7eaf5c0558ff4e29
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-08-05 12:33:38 +0200

    Update protectedarea.js

--- protectedarea.js
+++ protectedarea.js
@@ -1,2 +1,17 @@
-const JWT = require('jsonwebtoken')
+// checkpoint
+app.use((req, res, next) => {
+  const authorization = req.headers
+  jwt.verify(authorization, 'secret', (err, decodedToken) => {
+    if (err || !decodedToken) {
+      res.status(401).send('Not authorized')
+      return
+    }
+    next()
+  })
+})
 
+// endpoints
+app.get('/getusers', async (req, res) => {
+  const userList = await prisma.users()
+  res.send(userList)
+})
