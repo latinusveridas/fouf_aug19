59f6dad41c02ef1ba2ee3c85aaccd81260850bf9
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-08-05 12:48:30 +0200

    Update openarea.js

--- openarea.js
+++ openarea.js
@@ -1,3 +1,10 @@
+var express = require('express')
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/createquentin', async (req, res) => {
   const user = await prisma.createUser({name : "Quentin",password : "qwerty"})
   res.send(user)
