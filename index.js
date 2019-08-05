7d1337a5d95cf285424f2cdd0ddb77147f0451a6
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-08-05 12:34:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,4 @@ const bodyParser = require('body-parser')
 const PORT = 3000
 var app = express()
 
-app.get('/createquentin', async (req, res) => {
-  const user = await prisma.createUser({name : "Quentin",password : "qwerty"})
-  res.send(user)
-})
-
-app.get('/gettoken', async (req,res) => {
-  jwt.sign({password: 'x'}, 'thekey', {expiresIn: '12h'}, (err, token) => {
-    res.send(token)
-  })
-})
-
-app.use((req, res, next) => {
-  const authorization = req.headers
-  jwt.verify(authorization, 'secret', (err, decodedToken) => {
-    if (err || !decodedToken) {
-      res.status(401).send('Not authorized')
-      return
-    }
-    next()
-  })
-})
-
-app.get('/getusers', async (req, res) => {
-  const userList = await prisma.users()
-  res.send(userList)
-})
-
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)})
